353a7a2ace39e5726e95f426360ab7da55cb001e
thototh <144813512+thototh@users.noreply.github.com>
2026-04-03 12:24:31 +0900

    Update 3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    int x = 10; 
+    int y = 20;
+
+    printf("%d\n", x);
+
+    int *const ptr = &x;
+    *ptr = y;
+
+    printf("%d\n", x);
+
+    return 0;
+}
