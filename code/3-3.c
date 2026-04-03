4e07b1fbed8b3dd058b00517720612e46f3b4c84
thototh <144813512+thototh@users.noreply.github.com>
2026-04-03 12:26:47 +0900

    Update 3-3.c

---
+++ code/3-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    int val = 7;
+    int other = 77;
+
+    const int *const p = &val;
+    
+
+    printf("%d\n", *p);
+
+    return 0;
+}
